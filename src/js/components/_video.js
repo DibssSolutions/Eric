f742599496cf2cfb8370f2d8a3be59f159b45a41
Roman Muratov <myratov.r@gmail.com>
2018-12-20 15:04:00 +0200

    added fonts, header and hero

---
+++ src/js/components/_video.js
@@ -0,0 +1,13 @@
+import vide from 'vide';
+
+$(document).ready(function() {
+  setTimeout(() => {
+    const container = $('.js-video video').get(0);
+    if (container && container.paused) {
+      container.play();
+      console.log(container);
+    }
+    $(container).attr('playsinline','');
+
+  }, 1000);
+});
