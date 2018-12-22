efaf6cacdca32d63b8b7e9d4d01ab721c56759fb
Roman Muratov <myratov.r@gmail.com>
2018-12-22 16:26:13 +0200

    finished what-get, structure, schedule sections, header and hero mobile versions

--- src/js/components/_sandwich.js
+++ src/js/components/_sandwich.js
@@ -1,11 +1,15 @@
+import {ACTIVE, OPEN, BODY} from '../constants';
+
 const sandwich = $('.js-sandwich');
 const header = $('.js-header');
 $('.toggle-menu').on('click', function() {
-  sandwich.toggleClass('is-active');
-  header.toggleClass('is-open-menu');
+  sandwich.toggleClass(ACTIVE);
+  header.toggleClass(OPEN);
+  BODY.toggleClass('is-fixed');
 });
 
 $('.nav__link').on('click', function() {
-  sandwich.removeClass('is-active');
-  header.removeClass('is-open-menu');
+  sandwich.removeClass(ACTIVE);
+  header.removeClass(OPEN);
+  BODY.removeClass('is-fixed');
 });
--- src/js/components/index.js
+++ src/js/components/index.js
@@ -2,3 +2,5 @@ import './_video';
 import './_sandwich';
 import './_scrollbar';
 import './_slider';
+import './_tabs';
+import './_accordeon';
