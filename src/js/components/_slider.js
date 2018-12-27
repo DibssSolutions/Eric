f776dfcaf79e37a872e8deb922c092ce336afeee
Ilya khvesevich <rairetdev@gmail.com>
2018-12-27 18:02:15 +0200

    slider fixes

--- src/js/components/_slider.js
+++ src/js/components/_slider.js
@@ -1,9 +1,83 @@
 import slick from 'slick-carousel';
 
-$('.js-slider').slick({
+$('.js-slider-feedback').slick({
   infinite: true,
   slidesToShow: 1,
   slidesToScroll: 1,
   prevArrow: $('.js-slider-arrow-prev'),
-  nextArrow: $('.js-slider-arrow-next')
+  nextArrow: $('.js-slider-arrow-next'),
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.js-slider-arrow-prev'),
+        nextArrow: $('.js-slider-arrow-next'),
+        dots: true
+      }
+    }
+  ]
+});
+
+$('.js-slider-structure').slick({
+  dots: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        dots: true,
+        infinite: false,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        dots: true,
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    }
+  ]
+});
+
+$('.js-slider').slick({
+  dots: true,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
 });
